2073ae78687e45cd38a7c6a30ee5f438c5448367
Alex Levacher <levacher.alex@gmail.com>
2016-09-05 17:10:21 +0200

    fix nbsp and width:auto

--- src/combodate.js
+++ src/combodate.js
@@ -63,7 +63,7 @@
                 }
             }, this));
 
-            this.$widget.find('select').css('width', 'auto');
+            // this.$widget.find('select').css('width', 'auto');
 
             // hide original input and insert widget
             this.$element.hide().after(this.$widget);
@@ -90,7 +90,7 @@
             });
 
             //replace spaces with &nbsp;
-            tpl = tpl.replace(/ /g, '&nbsp;');
+            tpl = tpl.replace(/ /g, '');
 
             //second pass
             $.each(this.map, function(k, v) {
